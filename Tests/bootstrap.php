90a7879492656cd5dc5d046dd02dece747d1d152
Jonathan Dizdarevic <dizzda@gmail.com>
2013-06-24 23:47:28 +0200

    travis

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 if (!@include __DIR__ . '/../vendor/autoload.php') {
     die("You must set up the project dependencies, run the following commands:
-wget http://getcomposer.org/composer.phar
-php composer.phar install --dev
-");
+    wget http://getcomposer.org/composer.phar
+    php composer.phar install --dev
+    ");
 }
