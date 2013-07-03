af3e5852d88bf90352b57bb9c439b0835ac0a291
Jonathan Dizdarevic <dizzda@gmail.com>
2013-07-03 23:39:29 +0200

    test sandbox added

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,8 +1,10 @@
 <?php
 
-if (!@include __DIR__ . '/../vendor/autoload.php') {
-    die("You must set up the project dependencies, run the following commands:
-    wget http://getcomposer.org/composer.phar
-    php composer.phar install --dev
-    ");
+$file = __DIR__.'/../vendor/autoload.php';
+
+if(!file_exists($file))
+{
+    throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"');
 }
+
+require_once $file;
---
+++ Tests/sandbox/app/AppKernel.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace SRF\Bundles\CloudStackClientBundle\Tests\Functional\app;
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    /**
+     * @return array
+     */
+    public function registerBundles()
+    {
+        return array(
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\MonologBundle\MonologBundle(),
+            new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new \Dizda\CloudBackupBundle\DizdaCloudBackupBundle()
+        );
+    }
+
+    /**
+     * @return null
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return sys_get_temp_dir().'/DizdaCloudBackupBundle/cache';
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogDir()
+    {
+        return sys_get_temp_dir().'/DizdaCloudBackupBundle/logs';
+    }
+}
