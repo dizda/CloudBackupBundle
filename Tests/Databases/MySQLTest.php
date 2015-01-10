bd638ee9d47d7853e2de47202ba79d0bc56165eb
Jonathan Dizdarevic <dizzda@gmail.com>
2015-01-10 18:27:19 +0100

    fix tests in php5.3

--- Tests/Databases/MySQLTest.php
+++ Tests/Databases/MySQLTest.php
@@ -18,68 +18,68 @@ class MySQLTest extends AbstractTesting
     public function testGetCommand()
     {
         // dump all databases
-        $mysql = new MySQL([
-            'mysql' => [
+        $mysql = new MySQL(array(
+            'mysql' => array(
                 'all_databases' => true,
                 'db_host'     => 'localhost',
                 'db_port'     => 3306,
                 'database'    => 'dizbdd',
                 'db_user'     => 'root',
                 'db_password' => 'test'
-            ]
-        ], '/var/backup/');
+            )
+        ), '/var/backup/');
         $this->assertEquals($mysql->getCommand(), "mysqldump --host='localhost' --port='3306' --user='root' --password='test' --all-databases > /var/backup/mysql/all-databases.sql");
 
         // dump specified database
-        $mysql = new MySQL([
-            'mysql' => [
+        $mysql = new MySQL(array(
+            'mysql' => array(
                 'all_databases' => false,
                 'db_host'     => 'localhost',
                 'db_port'     => 3306,
                 'database'    => 'dizbdd',
                 'db_user'     => 'root',
                 'db_password' => 'test'
-            ]
-        ], '/var/backup/');
+            )
+        ), '/var/backup/');
         $this->assertEquals($mysql->getCommand(), "mysqldump --host='localhost' --port='3306' --user='root' --password='test' dizbdd > /var/backup/mysql/dizbdd.sql");
 
         // dump specified database
-        $mysql = new MySQL([
-            'mysql' => [
+        $mysql = new MySQL(array(
+            'mysql' => array(
                 'all_databases' => false,
                 'db_host'     => 'somehost',
                 'db_port'     => 2222,
                 'database'    => 'somebdd',
                 'db_user'     => 'mysql',
                 'db_password' => 'somepwd'
-            ]
-        ], '/var/backup/');
+            )
+        ), '/var/backup/');
         $this->assertEquals($mysql->getCommand(), "mysqldump --host='somehost' --port='2222' --user='mysql' --password='somepwd' somebdd > /var/backup/mysql/somebdd.sql");
 
         // dump specified database with no auth
-        $mysql = new MySQL([
-            'mysql' => [
+        $mysql = new MySQL(array(
+            'mysql' => array(
                 'all_databases' => false,
                 'db_host'     => 'somehost',
                 'db_port'     => 2222,
                 'database'    => 'somebdd',
                 'db_user'     => null,
                 'db_password' => null
-            ]
-        ], '/var/backup/');
+            )
+        ), '/var/backup/');
         $this->assertEquals($mysql->getCommand(), 'mysqldump  somebdd > /var/backup/mysql/somebdd.sql');
 
         // dump all databases with no auth
-        $mysql = new MySQL([
-            'mysql' => [
+        $mysql = new MySQL(array(
+            'mysql' => array(
                 'all_databases' => true,
                 'db_host'     => 'somehost',
                 'db_port'     => 2222,
                 'database'    => 'somebdd',
                 'db_user'     => null,
                 'db_password' => null
-            ]
-        ], '/var/backup/');
+            )
+        ), '/var/backup/');
         $this->assertEquals($mysql->getCommand(), 'mysqldump  --all-databases > /var/backup/mysql/all-databases.sql');
     }
 
