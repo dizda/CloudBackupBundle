261e28ab4e0f965889daf1a231a0946f5d50f591
Alexander <tim96@users.noreply.github.com>
2015-10-25 12:19:29 +0400

    Fix MySQLTest.

--- Tests/Database/MySQLTest.php
+++ Tests/Database/MySQLTest.php
@@ -25,7 +25,7 @@ class MySQLTest extends \PHPUnit_Framework_TestCase
                 'db_password' => 'test',
             ),
         ), '/var/backup/');
-        $this->assertEquals($mysql->getCommand(), "mysqldump --host='localhost' --port='3306' --user='root' --password='test' --all-databases > /var/backup/mysql/all-databases.sql");
+        $this->assertEquals($mysql->getCommand(), "mysqldump --host=\"localhost\" --port=\"3306\" --user=\"root\" --password=\"test\" --all-databases > /var/backup/mysql/all-databases.sql");
 
         // dump specified database
         $mysql = new MySQLDummy(array(
@@ -38,7 +38,7 @@ class MySQLTest extends \PHPUnit_Framework_TestCase
                 'db_password' => 'test',
             ),
         ), '/var/backup/');
-        $this->assertEquals($mysql->getCommand(), "mysqldump --host='localhost' --port='3306' --user='root' --password='test' dizbdd > /var/backup/mysql/dizbdd.sql");
+        $this->assertEquals($mysql->getCommand(), "mysqldump --host=\"localhost\" --port=\"3306\" --user=\"root\" --password=\"test\" dizbdd > /var/backup/mysql/dizbdd.sql");
 
         // dump specified database
         $mysql = new MySQLDummy(array(
@@ -51,7 +51,7 @@ class MySQLTest extends \PHPUnit_Framework_TestCase
                 'db_password' => 'somepwd',
             ),
         ), '/var/backup/');
-        $this->assertEquals($mysql->getCommand(), "mysqldump --host='somehost' --port='2222' --user='mysql' --password='somepwd' somebdd > /var/backup/mysql/somebdd.sql");
+        $this->assertEquals($mysql->getCommand(), "mysqldump --host=\"somehost\" --port=\"2222\" --user=\"mysql\" --password='\"somepwd\" somebdd > /var/backup/mysql/somebdd.sql");
 
         // dump specified database with no auth
         $mysql = new MySQLDummy(array(
