bd20d0aa75ea4c05160cfa89245ad19d1fa7b739
mctdev <info@mctdev.com>
2017-04-01 21:59:23 +0200

    PHP7.1 PHPUNIT 6

--- Tests/Manager/RestoreManagerTest.php
+++ Tests/Manager/RestoreManagerTest.php
@@ -107,7 +107,7 @@ class RestoreManagerTest extends \PHPUnit\Framework\TestCase
         $processorManagerMock = $this->getMockBuilder(ProcessorManager::class)->disableOriginalConstructor()->getMock();
         $processorManagerMock->expects($this->never())->method('uncompress');
         $eventDispatcherMock = $this->createMock(EventDispatcherInterface::class);
-		if(class_exists('PHPUnit_Framework_Constraint_Not'){
+		if(class_exists('PHPUnit_Framework_Constraint_Not')){
 			$eventDispatcherMock->expects($this->any())->method('dispatch')->with(
 				new \PHPUnit_Framework_Constraint_Not(RestoreEvent::RESTORE_COMPLETED)
 			);
